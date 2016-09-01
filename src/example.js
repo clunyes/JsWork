fb089d5dec1a32c8092e811789da286eb68ea848
clunyes <clunyes@126.com>
2016-09-01 17:42:30 +0800

    change some js

--- src/example.js
+++ src/example.js
@@ -1,4 +1,39 @@
 /**
  * Created by Administrator on 2016/9/1.
  */
+//自我提醒：有注释是好事
+/**
+ * 多行注释
+ */
+var mood = 'happy';
+var age = 'old burden';
+var MOOD;
+var names = ['zk', 'lxx', 'lq'];
+var beatles = Array('first', 'second', 'third');
+beatles[0] = names[2];
+//=========================
+var zhaokang = Object();
+zhaokang.name = 'zhaokang';
+zhaokang.age = 28;
+zhaokang.sex = '男'
 
+var zhaokang = {name: 'zhaokang', age: 28, sex: '男'};
+// alert(zhaokang.name + ' ' + zhaokang.age + ' ' + zhaokang.sex);
+
+//=======================
+// alert('20' + 20)
+
+//=======================
+// if (1 > 2) {
+//     alert('this world is changed')
+// } else {
+//     alert('ok, we know it')
+// }
+
+//=======================
+
+var myMood = 'happy';
+var yourMood = 'sad';
+if (myMood != yourMood) {
+    alert('our mood is different');
+}
