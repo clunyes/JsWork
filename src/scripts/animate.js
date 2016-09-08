3c590aa8d78d4f06c3b0cf107d88e1c5cf6f4c1a
clunyes <clunyes@126.com>
2016-09-08 14:12:46 +0800

    所有代码都是chrome上泡过的

--- src/scripts/animate.js
+++ src/scripts/animate.js
@@ -2,35 +2,51 @@
  * Created by Administrator on 2016/9/7.
  */
 function positionMessage() {
+    if (!document.getElementById('message'))return false;
     var elem = document.getElementById('message');
     elem.style.position = "absolute";
     elem.style.top = "100px";
     elem.style.left = "50px";
+    moveMessage("message", 200, 200, 20);
+    if (!document.getElementById('message2'))return false;
+    var elem = document.getElementById('message2');
+    elem.style.position = "absolute";
+    elem.style.top = "500px";
+    elem.style.left = "250px";
+    moveMessage("message2", 1000, 800, 20);
 }
 
-function moveMessage() {
-    var elem = document.getElementById('message');
+function moveMessage(elementId, final_x, final_y, interval) {
+    var elem = document.getElementById(elementId);
+    if (elem.movement) {
+        clearTimeout(elem.movement);
+    }
     var xpos = parseInt(elem.style.left);
     var ypos = parseInt(elem.style.top);
-    if (xpos == 200 && ypos == 500) {
+    var dist = 0;
+    if (xpos == final_x && ypos == final_y) {
         return true;
     }
-    if (xpos < 200) {
-        xpos++;
+    if (xpos < final_x) {
+        dist = Math.ceil((final_x - xpos) / 10);
+        xpos += dist;
     }
-    if (xpos > 200) {
-        xpos--;
+    if (xpos > final_x) {
+        dist = Math.ceil((xpos - final_x) / 10);
+        xpos -= dist;
     }
-    if (ypos < 500) {
-        ypos++;
+    if (ypos < final_y) {
+        dist = Math.ceil((final_y - ypos) / 10);
+        ypos += dist;
     }
-    if (ypos > 500) {
-        ypos--;
+    if (ypos > final_y) {
+        dist = Math.ceil((ypos - final_y) / 10);
+        ypos -= dist;
     }
     elem.style.left = xpos + "px";
     elem.style.top = ypos + "px";
-    var movement = setTimeout("moveMessage()", 10);
+    var repeat = "moveMessage('" + elementId + "','" + final_x + "','" + final_y + "','" + interval + "')";
+    elem.movement = setTimeout(repeat, interval);
 }
 
 addLoadEvent(positionMessage);
-addLoadEvent(moveMessage);
---
+++ src/scripts/list.js
@@ -0,0 +1,24 @@
+/**
+ * Created by Administrator on 2016/9/8.
+ */
+function prepareSlideShow() {
+    if (!document.getElementById("preview"))return false;
+    if (!document.getElementById("linklist"))return false;
+    var preview = document.getElementById('preview');
+    preview.style.position = "absolute";
+    preview.style.left = "0px";
+    preview.style.top = "0px";
+    var list = document.getElementById("linklist");
+    var links = document.getElementsByTagName("a");
+    links[0].onmouseover = function () {
+        moveMessage("preview", 0, 50, 10);
+    }
+    links[1].onmouseover = function () {
+        moveMessage("preview", 0, 100, 10);
+    }
+    links[2].onmouseover = function () {
+        moveMessage("preview", 0, 200, 10);
+    }
+}
+
+addLoadEvent(prepareSlideShow);
